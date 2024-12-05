8f4062fec283a37fd3c0f30bc51da8416c4760db
_txnq <117251703+tototonzq@users.noreply.github.com>
2024-12-06 01:40:20 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import { log } from 'console';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [],
 })
-export class AppComponent {
-  title = 'angular-playground';
+export class AppComponent implements OnInit {
+  public _http = inject(HttpClient);
+
+  constructor() {
+    // this._http.get('/api/test').subscribe((data) => log(data));
+  }
+
+  ngOnInit(): void {}
 }
